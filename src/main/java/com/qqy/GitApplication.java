2c140d4a4d90478a22d3c9891aa2a7d9d19c9c31
qiqy <qqy2363863225@163.com>
2019-12-17 20:55:34 +0800

    add www

--- src/main/java/com/qqy/GitApplication.java
+++ src/main/java/com/qqy/GitApplication.java
@@ -10,6 +10,7 @@ public class GitApplication {
         SpringApplication.run(GitApplication.class, args);
         System.out.println("init");
         System.out.println("123");
+        System.out.println("wwww");
     }
 
 }
