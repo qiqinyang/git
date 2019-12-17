96c23209299fc95d437e33a3c1d0fbb4b75a4980
qiqy <qqy2363863225@163.com>
2019-12-17 20:23:40 +0800

    sout

--- src/main/java/com/qqy/GitApplication.java
+++ src/main/java/com/qqy/GitApplication.java
@@ -8,6 +8,8 @@ public class GitApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitApplication.class, args);
+        System.out.println("init");
+        
     }
 
 }
